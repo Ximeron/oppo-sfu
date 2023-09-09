3c3c3603ce2c52be0faa25b6c0534eee201f3fe3
Ximeron <ximeron.274@gmail.com>
2023-09-09 13:40:15 +0700

    /

--- Weather bot/Weather bot/Weather bot.cpp
+++ Weather bot/Weather bot/Weather bot.cpp
@@ -1,56 +1,91 @@
-﻿// Weather bot.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
+
 
 
 
 
 using namespace std;
 
+
 struct weather {
-    string date;
+    struct date_split {
+        int day;
+        int month;
+        int year;
+    } s;
     string place;
     double temp;
-
 };
+
+int warn(ifstream& in) {
+    if (!in.is_open()) {
+        cout << "err";
+        return 0;
+    }
+}
+
+int splitter(string dte, char faq) {
+    string str = dte;
+    stringstream ss;
+    int dd, mm, yy;
+    ss << str;
+
+    if (faq == 'd') {
+        ss >> dd;
+        return dd;
+
+    }
+    else if (faq == 'm') {
+        ss.seekg(3);
+        ss >> mm;
+        return mm;
+
+    }
+    else if (faq == 'y') {
+        ss.seekg(6);
+        ss >> yy;
+        return yy;
+    }
+}
+
+Date getDate(istream& ist) {
+    Date date;
+    ist >> date.yyyy;
+    ist.get();
+    ist >> date.month;
+    ist.get();
+    ist >> date.day;
+    return date;
+}
+
+Weather getWeather(istream& ist) {
+    Weather weather;
+    ist >> weather.place;
+    weather.date = getDate(ist);
+    return weather;
+}
+
 
 
 int main() {
     vector<weather> weath;
     ifstream in("weather.txt");
-    if (!in.is_open()) {
-        cout << "err";
-        return 0;
-    }
+    
+    warn(in);
 
-    int num=0;
     
 
     while (!in.eof()) {
-        weather w;
-
-        in >> w.date >> w.place >> w.temp;
+        weather w = getWeather(in);
         weath.push_back(w);
-         
-        num++;
-
     }
 
     for (const auto& data:weath)  {
-        cout << data.date << " " << data.place << " " << data.temp << endl;
-        
-
+        cout << data.s.day << '.' << data.s.month << '.' << data.s.year << " " << data.place << " " << data.temp << endl;
     }
-       
-
-    in.close();
-
-
-    return 0;
-
-
 }
