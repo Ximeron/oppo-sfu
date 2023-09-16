99cfd43554b72346194a4d4bd805f272e6431932
Ximeron <96344505+Ximeron@users.noreply.github.com>
2023-09-16 08:52:58 +0700

    Update Weather bot.cpp

--- Weather bot/Weather bot/Weather bot.cpp
+++ Weather bot/Weather bot/Weather bot.cpp
@@ -4,88 +4,61 @@
 #include <string>
 #include <vector>
 #include <sstream>
-
-
-
-
-
 using namespace std;
 
-
-struct weather {
-    struct date_split {
-        int day;
-        int month;
-        int year;
-    } s;
-    string place;
-    double temp;
+struct Date {
+    int dd;
+    int mm;
+    int yy;
 };
 
-int warn(ifstream& in) {
+struct Weather {
+    struct Date d;
+    string place;
+    double temp;
+}; 
+
+
+
+int warn_if_file_not_open(ifstream& in) {
     if (!in.is_open()) {
         cout << "err";
         return 0;
     }
 }
 
-int splitter(string dte, char faq) {
-    string str = dte;
-    stringstream ss;
-    int dd, mm, yy;
-    ss << str;
-
-    if (faq == 'd') {
-        ss >> dd;
-        return dd;
-
-    }
-    else if (faq == 'm') {
-        ss.seekg(3);
-        ss >> mm;
-        return mm;
-
-    }
-    else if (faq == 'y') {
-        ss.seekg(6);
-        ss >> yy;
-        return yy;
-    }
-}
 
 Date getDate(istream& ist) {
     Date date;
-    ist >> date.yyyy;
+    ist >> date.dd;
     ist.get();
-    ist >> date.month;
+    ist >> date.mm;
     ist.get();
-    ist >> date.day;
+    ist >> date.yy;
     return date;
 }
 
 Weather getWeather(istream& ist) {
     Weather weather;
+    weather.d = getDate(ist);
     ist >> weather.place;
-    weather.date = getDate(ist);
+    ist >> weather.temp;
     return weather;
 }
 
 
 
 int main() {
-    vector<weather> weath;
+    vector<Weather> weath;
     ifstream in("weather.txt");
     
-    warn(in);
-
+    warn_if_file_not_open(in);
     
-
     while (!in.eof()) {
-        weather w = getWeather(in);
+        Weather w = getWeather(in);
         weath.push_back(w);
     }
-
     for (const auto& data:weath)  {
-        cout << data.s.day << '.' << data.s.month << '.' << data.s.year << " " << data.place << " " << data.temp << endl;
+        cout << data.d.dd << '.' << data.d.mm << '.' << data.d.yy << " " << data.place << " " << data.temp << endl;
     }
 }
