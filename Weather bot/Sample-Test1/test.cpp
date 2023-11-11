711a27a6a29312c3ce8e029e0e1356a33b94895a
Ximeron <96344505+Ximeron@users.noreply.github.com>
2023-11-11 12:17:04 +0700

    Update test.cpp

--- Weather bot/Sample-Test1/test.cpp
+++ Weather bot/Sample-Test1/test.cpp
@@ -6,26 +6,15 @@
 
 #include "../../Weather bot/Weather bot/weath.h"
 
-class TestD : public testing::Test {
-protected:
-	Date date = Date(1, 1, 1);
-	void setDay(int d);
-	void setMonth(int m);
-	void setYear(int y);
-	void setDate(int d, int m, int y) {
-		date.dd = d;
-		date.mm = m;
-		date.yy = y;
 
-	}
-	int runCheck() { return (D_check(date.dd, date.mm, date.yy) && Feb_test(date.dd, date.mm, date.yy)); }
-};
 
 
 
 TEST(DateCheck, Day) {
   EXPECT_EQ(1, 1);
-  ASSERT_TRUE(D_check(date.dd,date.mm,date.yy));
+  ASSERT_TRUE(D_check(20,07,2004));
+
+
 }
 
 
